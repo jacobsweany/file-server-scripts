53a859b774b9a164da76ec251917d4c351066608
jacobsweany <30737348+jacobsweany@users.noreply.github.com>
2018-07-31 17:40:20 -0700

    Update Run-TestNetworkSpeed.ps1

--- Run-TestNetworkSpeed.ps1
+++ Run-TestNetworkSpeed.ps1
@@ -1,19 +1,80 @@
-## Run-TestNetworkSpeed
-## Author: Jacob Sweany
-## Date: 6/28/2018
-##
-## This script will run the function Test-NetworkSpeed against a list of paths and append the results 
-## to a CSV file, save the output as a nicely formatted HTML table and email the results to listed recipients.
-## 
-## NOTE: This script will not run properly without populating $SharePath with the appropriate UNC path.
-##
-## Function was modified from this source:
-## http://community.spiceworks.com/scripts/show/2502-network-bandwidth-test-test-networkspeed-ps1
+<#
+.SYNOPSIS
+  Tests network speeds over multiple endpoints.
+.DESCRIPTION
+  Tests network speeds using Measure-Command and file copying. Script will generate random data ONLY at the source,
+  not at the endpoints.
+  Use the other script, Generate-RandomData.ps1 as locally as possible for each remote endpoint if possible if 
+  you are running traffic through WAN optimization.
 
+  Script will generate a CSV file, an HTML report, and a formatted email to be sent at each script run.
+  NOTE: This script will not run properly without populating "\\path" with appropriate UNC paths.
+.PARAMETER
+  None
+.INPUTS
+  None
+.OUTPUTS
+  None
+.NOTES
+  Version:        24.0
+  Author:         Jacob Sweany
+  Creation Date:  3/28/2018
+  Purpose/Change: Introduced Wait-forTCPDrop function.
+  
+  Function Test-NetworkSpeed was modified from this source:
+  http://community.spiceworks.com/scripts/show/2502-network-bandwidth-test-test-networkspeed-ps1
+
+  Function Generate-RandomData was modified from this source:
+  https://gallery.technet.microsoft.com/scriptcenter/Generate-random-binary-3e891264
+  
+.EXAMPLE
+  None
+#>
+
+
+
+# Log verbose transcript of every script run
+$VerbosePreference = "Continue"
+$LogPath = "\\path\to\logs"
+$LogPathName = Join-Path $LogPath -ChildPath "$($MyInvocation.ScriptName)-$(Get-Date -Format 'MM-dd-yyy').log"
+Start-Transcript $LogPathName -Append
+
+
+# Start initial script configuration
 $SourceServer = hostname
 Write-Warning "Hostname is $SourceServer"
 
-$SharePath = "\\path\to\share\"
+# Where to locally store test data
+$SharePath = "\\sharepath"
+$RunServer = ""
+
+# Toggle check for TCP drops between each test
+$RunTCPDropCheck = $true
+$DropCheckSeconds = 900
+
+#Paths to test
+$Paths = "
+\\path1,
+\\path2,
+\\path3"
+
+# Isilon IP ranges
+$isilon1 = 10..30 | foreach {"10.10.10.$_"}
+$isilon2 = 10..30 | foreach {"20.10.10.$_"}
+$isilon3 = 10..30 | foreach {"30.10.10.$_"}
+
+# Combine Isilon IP ranges with all servers used in test, remove duplicates
+$iparray = New-Object psobject@{}
+foreach ($path in $Paths) {
+    $Server = $path.Split("\")[2]
+    $ip = [System.Net.Dns]::GetHostAddresses("$Server").IPAddressToString
+    [array]$iparray += $ip
+}
+$all_IPs = $isilon1 + $isilon2 + $isilon3 + $iparray | select -Unique
+
+# End initial script configuration
+
+$ScriptStartTime = Get-Date
 
 # Check for a lockfile, to avoid more than one speed test at one time. If lockfile is detected, terminate script.
 # This is not needed if only one test is run in the environment.
@@ -25,44 +86,38 @@ if ($LockFileCheck) {
 # Create lockfile
 Write-Warning "Creating lockfile for $SourceServer at $(Get-Date)" | Out-File "$SharePath\$SourceServer.lockfile" -Force
 
+## Functions
+
+function Wait-forTCPDrop {
+    [CmdletBinding()]
+    Param(
+    [int]$TimeOutSeconds,
+    [int]$RemotePort,
+    [array]$CheckIPs
+    )
+    $RunOutTimer = New-TimeSpan -Seconds $TimeOutSeconds
+    $Stopwatch = [Diagnostics.Stopwatch]::StartNew()
+    $Check = Get-NetTCPConnection -State Established -RemotePort $RemotePort -RemoteAddress $CheckIPs -ErrorAction SilentlyContinue | select -Unique
+    while ($Check.State -eq "Established") {
+        $Check = Get-NetTCPConnection -State Established -RemotePort $RemotePort -RemoteAddress $CheckIPs -ErrorAction SilentlyContinue | select -Unique
+        Write-Verbose "Waiting for TCP session for $($Check.RemoteAddress | select -Unique) port $($check.RemotePort | select -Unique) to drop. $($Stopwatch.Elapsed.Minutes) mins $($Stopwatch.Elapsed.Seconds) seconds elapsed."
+        if ($Stopwatch.Elapsed -ge $RunOutTimer) {
+            Write-Warning "$TimeOutSeconds seconds have elapsed. Giving up TCP drop window wait"
+            return
+        }
+        Start-Sleep -Seconds 1
+    }
+    Write-Host "$($Stopwatch.Elapsed.Minutes) mins $($Stopwatch.Elapsed.Seconds) seconds elapsed total."
+    $Stopwatch.Stop()
+    Remove-Variable -Name Check
+}
 
 function Generate-RandomData {
-    <# 
-    .Synopsis 
-      Generate files with random binary data. 
-    .DESCRIPTION 
-      Random files are useful for testing synchronization processes, backup/restore 
-      and anything in general that handles large quantities of file data. 
- 
-      This script specifically does not create sparse files, or files with zeros or 
-      other constant data. Such data is easily compressible or can be optimize during transfer. 
-      Instead, the script generates pseudo-random data that cannot be optimized. The .NET  
-      random generator used here is quite fast with about 100 MB/s. Writing the data 
-      is usually the bottleneck.  
- 
-      Some notes on usage: by default, about 15 files, 100 MB total is generated in the  
-      current directory. If you specify a TargetPath, it must exist. File sizes are random betwee 
-      Minfilesize and Maxfilesize; these are allowed to be equal, generating fixed-size files.  
-      Also, the file timestamps are randomized between <now> and 24h ago, to give 
-      synchronization algorithms something to work with. Filenames are generated by 
-      randomly shuffling the "filenameseed" string, and always end with ".bin".  
-    .EXAMPLE 
-      .\Generate-RandomBinaryFiles -Targetpath c:\temp\Randomdata 
-    .EXAMPLE 
-      .\Generate-RandomBinaryFiles -Targetpath c:\temp\Randomdata -minfilesize 100MB -maxfilesize 100MB -totalsize 10GB -timerangehours 0 
-    .NOTES 
-        Version:        1.0 : first version.  
-        Author:         Willem Kasdorp, Microsoft.  
-        Creation Date:  1/10/2017 
-        Last modified: 
-    #> 
-
     [CmdletBinding()] 
     Param( 
         [String] $TargetPath = "", 
         [int64] $filesize = 512MB, 
-        [int] $timerangehours = 0 
-        #[string] $filenameseed = "abcdefghijkl012345"    
+        [int] $timerangehours = 0   
     ) 
  
     # 
@@ -110,7 +165,7 @@ function Generate-RandomData {
         $fileobject.CreationTime = $timestamp 
         $fileobject.LastWriteTime = $timestamp 
  
-        # show what we did.  
+        # show results  
         [pscustomobject] @{ 
             filename = $path_gen 
             timestamp = $timestamp 
@@ -118,13 +173,11 @@ function Generate-RandomData {
         } 
     } catch { 
         $message = "failed to write data to $TargetPath, error $($_.Exception.Message)" 
-        Throw $message 
+        throw $message 
     }      
 
 }
 
-
-## Function definition
 function Test-NetworkSpeed {
     <#
     .SYNOPSIS
@@ -168,22 +221,35 @@ function Test-NetworkSpeed {
     [CmdletBinding()]
     Param (
         [Parameter(Mandatory,ValueFromPipeline,HelpMessage="Enter UNC's to server to test (dummy file will be saved in this path)")]
-        [String[]]$Path
+        [String[]]$Path,
+        [String]$ColdRun
         #[ValidateRange(1,1000)]
         #[int]$Size = 10,
-        #[String[]]$SharePath
     )
 
     Begin {
-        Write-Verbose "$(Get-Date): Test-NetworkSpeed Script begins"
-        Write-Verbose "$(Get-Date): Create dummy file, Size: $($Size)MB"
-        $Source = "c:\temp"
-        Remove-Item $Source\Test.txt -ErrorAction SilentlyContinue -Force
+        if ($ColdRun -eq "ColdRun") {$IsColdRun = $true}
+        if ($ColdRun -eq "WarmRun") {$IsColdRun = $false}
+
+        Write-Verbose "$(Get-Date): Test-NetworkSpeed Script begins for $Path, ColdRun = $ColdRun"
+        #Write-Verbose "$(Get-Date): Create dummy file, Size: $($Size)MB"
+        $Source = "\\path"
+        $SourceServer = hostname
         Set-Location $Source
-        #$DummySize = $Size * 1048576
-        $CreateMsg = Generate-RandomData -TargetPath "$Source\test.txt"
-        #$CreateMsg = fsutil file createnew test.txt $DummySize
-        
+
+        # COLD RUN
+        # If cold run, remove local data file then generate random source file
+        if ($IsColdRun) {
+            Write-Verbose "$(Get-Date): COLD RUN: remove local data file then generate random source file, $Source\Test$RunServer.txt"
+            Remove-Item $Source\Test$RunServer.txt -ErrorAction SilentlyContinue -Force
+            $CreateMsg = Generate-RandomData -TargetPath "$Source\Test$RunServer.txt"
+        }
+        # WARM RUN
+        # Data file is kept from last cold run, so no new file is generated
+        if (!$IsColdRun) {
+            Write-Verbose "$(Get-Date): WARM RUN: Data file is kept from last cold run, so no new file is generated"
+        }
+
         # See if data file exists
         Try {
             $TotalSize = (Get-ChildItem $Source\test.txt -ErrorAction Stop).Length
@@ -196,10 +262,10 @@ function Test-NetworkSpeed {
             Remove-Item -Path "$Path\$SourceServer.lockfile" -Force -ErrorAction SilentlyContinue
             Exit
         }
-        Write-Verbose "$(Get-Date): Source for dummy file: $Source\test.txt"
+        Write-Verbose "$(Get-Date): Source for dummy file: $Source\Test$RunServer.txt"
         $RunTime = Get-Date
     }
-
+    # Main test process
     Process {
         ForEach ($ServerPath in $Path)
         {   $Server = $ServerPath.Split("\")[2]
@@ -229,22 +295,62 @@ function Test-NetworkSpeed {
             }
             
             Try {
-                
+                ##
+                ##
                 # Write test
-                Write-Verbose "$(Get-Date): Write Test..."
+                ##
+                ##
+                # Copy data from local folder to target folder
+                Write-Verbose "$(Get-Date): WRITE TEST: copy data from local $Source to remote $Target"
                 $WriteTest = Measure-Command { 
-                    Copy-Item $Source\Test.txt $Target -ErrorAction Stop
+                    Copy-Item $Source\Test$RunServer.txt $Target -ErrorAction Stop -Force
                 }
-                Remove-Item $Target\Test.txt -ErrorAction SilentlyContinue
+                # Remove copied data from from target
+                Write-Verbose "$(Get-Date): WRITE TEST: remove $Target\Test$RunServer.txt"
+                Remove-Item $Target\Test$RunServer.txt -ErrorAction SilentlyContinue
+                
+                # Wait for TCP session drop
+                if ($RunTCPDropCheck) {
+                    Wait-forTCPDrop -TimeOutSeconds $DropCheckSeconds -RemotePort 445 -CheckIPs $all_IPs -Verbose
+                }
+                
+                ##
+                ##
                 # Read test
-                Write-Verbose "$(Get-Date): Read Test..."
-                # Identify data file from target to copy
-                $RandomFile = Get-ChildItem -Path "$Target\data\*.bin" | Select-Object -First 1
-                Write-Verbose "Random file selected: $RandomFile"
-                $ReadTest = Measure-Command {
-                    Copy-Item $RandomFile $Source\TestRead.txt -ErrorAction Stop
+                ##
+                ##
+                Write-Verbose "$(Get-Date): READ TEST: Copy data from remote $Target to local $Source"
+
+                # COLD RUN
+                # If cold run, copy unique file from target to local, delete local file after copied,
+                # but keep original target file.
+                if ($IsColdRun) {
+                    Write-Verbose "$(Get-Date): READ TEST: COLD RUN: copy unique file from target to local, delete local file after copied, but keep original target file."
+                    # Identify data file from target to copy
+                    $RandomFile = Get-ChildItem -Path "$Target\data\*.bin" | Select-Object -First 1
+                    Write-Verbose "$(Get-Date): READ TEST: COLD RUN: Unique file selected: $RandomFile"
+                    $RandomFile | select -ExpandProperty Name | Out-File "$Target\data\selected.txt" -Force
+                    $ReadTest = Measure-Command {
+                        Copy-Item $RandomFile $Source\TestRead.txt -ErrorAction Stop -Force
+                    }
+                    Write-Verbose "$(Get-Date): READ TEST: COLD RUN: Removing $Source\TestRead$RunServer.txt"
+                    Remove-Item $Source\TestRead$RunServer.txt -ErrorAction SilentlyContinue -Force
                 }
-                Remove-Item $RandomFile -ErrorAction SilentlyContinue -Force
+                # WARM RUN
+                # If warm run, grab same unique random file from last cold run, copy from target to source,
+                # delete file copied to source, and delete original source
+                if (!$IsColdRun) {
+                    Write-Verbose "$(Get-Date): READ TEST: WARM RUN: grab same unique random file from last cold run, copy from target to source, delete file copied to source, and delete original source"
+                    $RandomFile = Get-Content "$Target\data\selected.txt"
+                    Write-Verbose "$(Get-Date): READ TEST: WARM RUN: Random file selected from last cold run: $RandomFile"
+                    $ReadTest = Measure-Command {
+                        Copy-Item $Target\data\$RandomFile $Source\TestRead$RunServer.txt -ErrorAction Stop -Force
+                    }
+                    Write-Verbose "$(Get-Date): READ TEST: WARM RUN: Removing $Target\data\$RandomFile"
+                    Remove-Item $Target\data\$RandomFile -ErrorAction SilentlyContinue -Force
+                    Write-Verbose "$(Get-Date): READ TEST: WARM RUN: Removing $Source\TestRead$RunServer.txt"
+                    Remove-Item $Source\TestRead$RunServer.txt -ErrorAction SilentlyContinue -Force
+                }
                 $Status = "OK"
                 $WriteMbps = [Math]::Round((($TotalSize * 8) / $WriteTest.TotalSeconds) / 1048576,2)
                 $ReadMbps = [Math]::Round((($TotalSize * 8) / $ReadTest.TotalSeconds) / 1048576,2)
@@ -259,23 +365,25 @@ function Test-NetworkSpeed {
             $Output = [PSCustomObject]@{
                 Server = $Server
                 TimeStamp = $RunTime
-                Status = "OK"
+                Status = $Status
                 WriteTime = $WriteTest
                 WriteMbps = $WriteMbps
                 ReadTime = $ReadTest
                 ReadMbps = $ReadMbps
                 SourceServer = $SourceServer
                 Size = $Size
+                ColdRun = $ColdRun
             }
-            
-            Remove-Item $Source\TestRead.txt -ErrorAction SilentlyContinue
             return $Output
         }
     }
 
     End {
-        Remove-Item $Source\Test.txt -Force -ErrorAction SilentlyContinue
-        Write-Verbose "$(Get-Date): Test-NetworkSpeed completed!"
+        if (!$IsColdRun) {
+            Write-Verbose "$(Get-Date): WARM RUN: Removing $Source\Test$RunServer.txt"
+            Remove-Item $Source\Test$RunServer.txt -Force -ErrorAction SilentlyContinue
+        }
+        Write-Verbose "$(Get-Date): Test-NetworkSpeed complete for $Path, ColdRun = $ColdRun"
     }
 }
 
@@ -285,13 +393,11 @@ function Test-NetworkSpeed {
 $RunTimes = 2
 
 # Where to email the report to after test is complete
-#$Recpients = @('email@test.com', 'email2.test.com')
+$Recpients = @('email@test.com', 'email2.test.com')
 
 # RunBank array will store each run of each test, output is a table when done
 $RunBank = New-Object psobject @{}
 # Define each path to test
-$Paths = 
-"$SharePath"
 
 # Define CSV file for logging each speed test run
 $LogCsv = "$SharePath\NetworkSpeedTests.csv"
@@ -306,12 +412,34 @@ if (!(Test-Path $LogCsv)) {
 # All speed test actions are here. Each test will repeat $RunTimes, each test will be logged to CSV and $RunBank array for HTML/email export.
 for ($i=1; $i -le $RunTimes; $i++) {
     foreach ($path in $Paths) {
-        # Speed test action
-        $TestRun = Test-NetworkSpeed -Path $path -Verbose
+        # Speed test: Cold run
+        $TestRun = Test-NetworkSpeed -Path $path -ColdRun "ColdRun" -Verbose
+        $TestRun
         # Append data to CSV file
         $TestRun | Export-Csv -Path $LogCsv -Append -NoTypeInformation
         # Add test results to $RunBank array
         [array]$RunBank += $TestRun
+        
+        # wait for TCP session drop
+        if ($RunTCPDropCheck) {
+            Wait-forTCPDrop -TimeOutSeconds $DropCheckSeconds -RemotePort 445 -CheckIPs $all_IPs -Verbose
+        }
+        # Speed test: Warm run
+        $TestRun = Test-NetworkSpeed -Path $path -ColdRun "WarmRun" -Verbose
+        $TestRun
+        # Append data to CSV file
+        $TestRun | Export-Csv -Path $LogCsv -Append -NoTypeInformation
+        # Add test results to $RunBank array
+        [array]$RunBank += $TestRun
+        
+        # wait for TCP session drop
+        if ($RunTCPDropCheck) {
+            Wait-forTCPDrop -TimeOutSeconds $DropCheckSeconds -RemotePort 445 -CheckIPs $all_IPs -Verbose
+        }
+
+        # Wait additional minute between passes
+        Write-Verbose "Waiting additional minute between passes.."
+        Start-Sleep -Seconds 60
     }
 }
 
@@ -319,9 +447,19 @@ for ($i=1; $i -le $RunTimes; $i++) {
 #$RunBank | select Server, TimeStamp, WriteMbps, ReadMbps, SourceServer, Size | Export-Csv -Path $LogCsv -NoTypeInformation -Force -Append
 
 ## HTML formatting/email creation
-$table = $RunBank | select Server, TimeStamp, WriteMbps, ReadMbps, SourceServer, Size | ConvertTo-Html -Fragment
+$table = $RunBank | select Server, TimeStamp, WriteMbps, ReadMbps, SourceServer, Size, ColdRun | ConvertTo-Html -Fragment
+
+$ScriptEndTime = Get-Date
+$ScriptTotalRunTime = New-TimeSpan -Start $ScriptStartTime -End $ScriptEndTime
+
+if ($ScriptTotalRunTime.Hours -gt 0) {$ScriptHrs = "$($ScriptTotalRunTime.Hours) Hours, "}
+if ($ScriptTotalRunTime.Minutes -gt 0) {$ScriptMins = "$($ScriptTotalRunTime.Minutes) Minutes, "}
+
+$ScriptTotalRunTimeText = "$ScriptHrs$ScriptMins$($ScriptTotalRunTime.Seconds) Seconds"
+
+
 $Title = "Speed Test Run: $(Get-Date)"
-$ReportDescription = "Description for report goes here"
+$ReportDescription = "Description for report goes here. <br/> <b>Total runtime: $ScriptTotalRunTimeText</b>"
 # Define HTML head/styles
 $Head = @"
 <Title>$Title</Title>
@@ -343,7 +481,7 @@ table { width: 95%; margin-left: 5px; margin-bottom: 20px; }
 [string]$emailBody = ConvertTo-Html -Head $Head -Body $table | Out-File "$SharePath\report.htm" -Encoding ascii -Force
 
 # Send email - make sure to populate "From" and "SmtpServer" fields
-#Send-MailMessage -BodyAsHtml -From "noreply@test.com" -Subject $Title -Body $emailBody -To $Recpients -SmtpServer "mail.test.com"
+Send-MailMessage -BodyAsHtml -From "noreply@test.com" -Subject $Title -Body $emailBody -To $Recpients -SmtpServer "mail.test.com"
 
 # !important Remove lock file on exit
 Remove-Item -Path "$SharePath\$SourceServer.lockfile" -Force -ErrorAction SilentlyContinue
